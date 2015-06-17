6d36d8edd4062864cf3c847a9277dd5b2c1d0511
unknown <Stanislav.Hamara@INT-STANISLAVH.red-gate.com>
2015-06-17 14:07:42 +0100

    Created Money class

---
+++ BankingKata/Money.cs
@@ -0,0 +1,13 @@
+﻿namespace BankingKata
+{
+    public class Money
+    {
+
+        private readonly int value;
+        public Money(int i)
+        {
+            this.value = i;
+        }
+
+    } 
+}
