a4472deb56abe34b7cee0f4e73dd79cfe51da135
Stanislav Hamara <stanislav.hamara@red-gate.com>
2015-06-22 09:46:11 +0100

    test if withdraw money decreases balance

--- BankingKataTests/AccountTests.cs
+++ BankingKataTests/AccountTests.cs
@@ -10,7 +10,6 @@ namespace BankingKataTests
 {
 	[TestFixture]
     public class AccountTests
-    {
 	{
 	    private Account account;
 
@@ -23,13 +22,22 @@ namespace BankingKataTests
 	    [Test]
 	    public void DepositingMoneyIntoAnAccountIncreasesTheBalanceByThatAmount()
 	    {
-	        var account = new Account();
-
 	        account.Deposit(new Money(2));
 	        var balance = account.CalculateBalance();
 
 	        var expectedBalance = new Money(2);
 	        Assert.That(balance, Is.EqualTo(expectedBalance));
+	    }
+
+	    [Test]
+	    public void WithdrawingMoneyFromAccountDecreasesTheBalanceByThatAmount()
+	    {
+            account.Deposit(new Money(5));
+	        account.Withdraw(new Money(3));
+	        var balance = account.CalculateBalance();
+
+            var expectedBalance = new Money(2);
+            Assert.That(balance, Is.EqualTo(expectedBalance));
 	    }
     }
 
