5d5c1cd70706bf112844f7c17bcd296f7bdd288b
Stanislav Hamara <stanislav.hamara@red-gate.com>
2015-06-22 09:08:17 +0100

    create `AccountTest` class

---
+++ BankingKataTests/AccountTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingKata;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+	[TestFixture]
+    public class AccountTests
+    {
+	    [Test]
+	    public void DepositingMoneyIntoAnAccountIncreasesTheBalanceByThatAmount()
+	    {
+	        var account = new Account();
+
+	        account.Deposit(new Money(2));
+	        account.Deposit(new Money(3));
+	        account.Deposit(new Money(5));
+	        var balance = account.CalculateBalance();
+
+	        var expectedBalance = new Money(10);
+	        Assert.That(balance, Is.EqualTo(expectedBalance));
+	    }
+    }
+
+}
