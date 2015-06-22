9fafbee2c4277edbab7172ef1f1229890b5f235c
Stanislav Hamara <stanislav.hamara@red-gate.com>
2015-06-22 09:06:44 +0100

    create `Account` class

---
+++ BankingKata/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingKata
+{
+    public class Account
+    {
+        public void Deposit(Money p0)
+        {
+            
+        }
+
+        public object CalculateBalance()
+        {
+            return null;
+        }
+    }
+}
